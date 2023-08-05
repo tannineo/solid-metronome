9572e4eb8cd337888fcdfebdc11aaa36bfba63b5
tannineo <tannineo@outlook.com>
2023-08-05 12:57:16 +0100

    feat: button click effect

--- src/components/metronome/BaseButton.tsx
+++ src/components/metronome/BaseButton.tsx
@@ -11,6 +11,7 @@ const BaseButton = (props: ParentProps<{ onClick?: () => any; mini?: boolean }>)
         props.mini ? 'w-10 h-10' : 'w-20 h-20',
       )}
       u-flex='~ items-center justify-center'
+      u-active='transition-all ease-in-out duration-100 ring-ai ring-offset-6 ring-offset-transparent ring-4'
       onClick={props.onClick}
     >
       {c()}
