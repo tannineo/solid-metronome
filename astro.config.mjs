8e2702d086b913f7c70d2f71beb998fb69e22cde
tannineo <tannineo@outlook.com>
2023-10-02 15:08:04 +0100

    chore(build): rid of react pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
-import solidJs from '@astrojs/solid-js';
-import mdx from '@astrojs/mdx';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+import solidJs from '@astrojs/solid-js'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: true
-  }), solidJs(), mdx(), react()],
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    solidJs(),
+    mdx(),
+  ],
   // deploy to github pages
   site: 'https://tannineo.github.io',
-  base: '/solid-metronome'
-});
+  base: '/solid-metronome',
+})
